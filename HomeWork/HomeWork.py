775f4ba8b2a2fe2c7538a33af40344d4e3426586
Евгений <geniy15049999@gmail.com>
2023-03-25 09:50:30 +0300

    hm3

--- HomeWork/HomeWork.py
+++ HomeWork/HomeWork.py
@@ -10,13 +10,13 @@
 """
 import math
 print("Задача 1\n")
-seasons_list = ['winter', 'spring', 'summer', 'autumn']
-seasons_dict = {0: 'winter', 1: 'spring', 2: 'summer', 3: 'autumn'}
+seasonsList = ['winter', 'spring', 'summer', 'autumn']
+seasonsDict = {0: 'winter', 1: 'spring', 2: 'summer', 3: 'autumn'}
 month = int(input("Введите номер месяца: "))
 month = month + 1 if month < 12 else 1
-season_number = math.ceil(month / 3) - 1
-print(seasons_list[season_number])
-print(seasons_dict[season_number])
+seasonNumber = math.ceil(month / 3) - 1
+print(seasonsList[seasonNumber])
+print(seasonsDict[seasonNumber])
 
 """
 2. Реализовать функцию, принимающую два числа (позиционные аргументы)
